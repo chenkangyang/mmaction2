5756fd33da61448641d1fc85db1229e0cbfe14fb
chenkangyang <chenkangyang98@gmail.com>
2021-08-08 11:57:04 +0800

    kychen's video-swin-trans

--- mmaction/datasets/pipelines/__init__.py
+++ mmaction/datasets/pipelines/__init__.py
@@ -1,6 +1,6 @@
 from .augmentations import (AudioAmplify, CenterCrop, ColorJitter, Flip, Fuse,
                             Imgaug, MelSpectrogram, MultiGroupCrop,
-                            MultiScaleCrop, Normalize, RandomCrop,
+                            MultiScaleCrop, Normalize, RandomCrop, RandomErasing,
                             RandomRescale, RandomResizedCrop, RandomScale,
                             Resize, TenCrop, ThreeCrop, TorchvisionTrans)
 from .compose import Compose
@@ -20,7 +20,7 @@ from .pose_loading import (GeneratePoseTarget, LoadKineticsPose, PoseDecode,
 
 __all__ = [
     'SampleFrames', 'PyAVDecode', 'DecordDecode', 'DenseSampleFrames',
-    'OpenCVDecode', 'MultiGroupCrop', 'MultiScaleCrop', 'RandomResizedCrop',
+    'OpenCVDecode', 'MultiGroupCrop', 'MultiScaleCrop', 'RandomResizedCrop', 'RandomErasing'
     'RandomCrop', 'Resize', 'Flip', 'Fuse', 'Normalize', 'ThreeCrop',
     'CenterCrop', 'TenCrop', 'ImageToTensor', 'Transpose', 'Collect',
     'FormatShape', 'Compose', 'ToTensor', 'ToDataContainer',
